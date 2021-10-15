3bf79fa49210340b8b0afe062fae9b085de238c1
Valeria Martinez <valemr31@hotmail.com>
2021-10-14 21:41:41 -0600

    Req 4

--- Progra1POO/src/Controlador/ConRegistroPlanEstudio.java
+++ Progra1POO/src/Controlador/ConRegistroPlanEstudio.java
@@ -1,6 +1,7 @@
 package Controlador;
 
 import Vistas.Menu;
+import java.sql.Date;
 
 /**
  *
@@ -8,11 +9,18 @@ import Vistas.Menu;
  */
 public class ConRegistroPlanEstudio {
 
-   static Vistas.Menu menu = new Menu();
-    
+    static Vistas.Menu menu = new Menu();
+
     //llama a la ventana anterior 
     public static void Regresar() {
         menu.setVisible(true);
         Controlador.ConMenu.registroPlanEstudio.setVisible(false);
+    }
+
+    public static void Registrar(String codigoEscuela, int numPlan, Date fechaVigencia) {
+        String consulta1 = "Insert into PlanEstudio (numPlan, fechaVigencia) values ('" + numPlan + "','" + fechaVigencia + "');";
+        Modelo.ConexionSQL.consultaRegistar(consulta1);
+        String consulta2 = "Insert into Escuela_PlanEstudio (codigoEscuela, numPlan) values ('" + codigoEscuela + "','" + numPlan + "');";
+        Modelo.ConexionSQL.consultaRegistar(consulta2);
     }
 }
