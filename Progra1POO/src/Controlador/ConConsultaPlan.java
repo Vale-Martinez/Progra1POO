5a1a0283a9cf844afa549825f296fb338e3c44f8
Valeria Martinez <valemr31@hotmail.com>
2021-10-16 18:56:29 -0600

    Req 5 pt 2

--- Progra1POO/src/Controlador/ConConsultaPlan.java
+++ Progra1POO/src/Controlador/ConConsultaPlan.java
@@ -1,8 +1,28 @@
 package Controlador;
 
+import Vistas.EnvioPDF;
 import Vistas.Menu;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
@@ -13,6 +33,10 @@ import javax.swing.table.DefaultTableModel;
 public class ConConsultaPlan {
 
     static Vistas.Menu menu = new Menu();
+    static Vistas.EnvioPDF envioPDF = new EnvioPDF();
+
+    static int numPlanC, totalCreditosC, totalCursosC;
+    static String codigoEscC;
 
     //llama a la ventana anterior 
     public static void Regresar() {
@@ -20,8 +44,11 @@ public class ConConsultaPlan {
         Controlador.ConMenu.consultaPlan.setVisible(false);
     }
 
-    public static void ConsultarPlan(int numPlan, String CodigoEsc) {
+    public static void ConsultarPlan(int numPlan, String codigoEsc) {
         try {
+            numPlanC = numPlan;
+            codigoEscC = codigoEsc;
+
             DefaultTableModel model = (DefaultTableModel) Vistas.ConsultaPlan.tblConsultaPlan.getModel();
             model.setRowCount(0);
             String consulta = "Select distinct Cur.codigoCurso,cur.nombreCurso,cur.bloque ,cur.horasLectivas,cur.creditos\n"
@@ -31,8 +58,8 @@ public class ConConsultaPlan {
                     + "ON (Escuela_PlanEstudio.numPlan = PlanEstudio_Curso.numPlan)\n"
                     + "inner join Curso cur\n"
                     + "ON (PlanEstudio_Curso.codigoCurso = Cur.codigoCurso)"
-                    + "WHERE Escuela_PlanEstudio.numPlan =" + numPlan+" "
-                    + "AND escuela.codigoEscuela ='"+ CodigoEsc+"';";
+                    + "WHERE Escuela_PlanEstudio.numPlan =" + numPlan + " "
+                    + "AND escuela.codigoEscuela ='" + codigoEsc + "';";
             ResultSet res = Modelo.ConexionSQL.consulta(consulta);
             while (res.next()) {
                 Vector v = new Vector();
@@ -44,9 +71,148 @@ public class ConConsultaPlan {
                 model.addRow(v);
                 Vistas.ConsultaPlan.tblConsultaPlan.setModel(model);
             }
+
+            String consulta2 = "Select distinct count (Cur.codigoCurso) as totalCursos, sum (cur.creditos) as totalCreditos\n"
+                    + "from Escuela inner join Escuela_PlanEstudio \n"
+                    + "ON (Escuela.codigoEscuela = Escuela_PlanEstudio.codigoEscuela)\n"
+                    + "inner join PlanEstudio_Curso \n"
+                    + "ON (Escuela_PlanEstudio.numPlan = PlanEstudio_Curso.numPlan)\n"
+                    + "inner join Curso cur\n"
+                    + "ON (PlanEstudio_Curso.codigoCurso = Cur.codigoCurso)"
+                    + "WHERE Escuela_PlanEstudio.numPlan =" + numPlan + " "
+                    + "AND escuela.codigoEscuela ='" + codigoEsc + "';";
+            ResultSet res2 = Modelo.ConexionSQL.consulta(consulta2);
+            while (res2.next()) {
+                Vector v = new Vector();
+                v.add(res2.getInt(1));
+                v.add(res2.getInt(2));
+                Vistas.ConsultaPlan.LblTotalCursos.setText(v.get(0).toString());
+                totalCursosC = Integer.parseInt(v.get(0).toString());
+                Vistas.ConsultaPlan.LblTotalCreditos.setText(v.get(1).toString());
+                totalCreditosC = Integer.parseInt(v.get(1).toString());
+            }
+
         } catch (Exception e) {
-             JOptionPane.showMessageDialog(null, "Error" + e.getMessage(), "Error conexion", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Error " + e.getMessage(), "Error conexion", JOptionPane.ERROR_MESSAGE);
         }
 
+    }
+
+    public static void AbrirEnviarPDF() {
+        envioPDF.setVisible(true);
+    }
+
+    //crea el pdf para la factura
+    private static String Crearpdf() throws SQLException {
+
+        //crea la direccion donde se va a guardar el pdf y como se va a llamar y su contenido 
+        String nombreDoc = "Plan " + numPlanC;
+        String Path = "C:\\Users\\valem\\Desktop\\TEC\\2021\\POO proyectos\\Progra 1\\" + nombreDoc;
+        String encabezado = "Plan de estudios = " + numPlanC + "\n"
+                + "Codigo Escuela a cargo = " + codigoEscC + "\nTotal de cursos = " + totalCursosC + "\n"
+                + "Total de creditos = " + totalCreditosC + "\n ";
+
+        String cursos = "[Codigo Curso, Nombre Curso, Bloque, Horas Lectivas, Creditos] \n";
+
+        String consulta = "Select distinct Cur.codigoCurso,cur.nombreCurso,cur.bloque ,cur.horasLectivas,cur.creditos\n"
+                + "from Escuela inner join Escuela_PlanEstudio \n"
+                + "ON (Escuela.codigoEscuela = Escuela_PlanEstudio.codigoEscuela)\n"
+                + "inner join PlanEstudio_Curso \n"
+                + "ON (Escuela_PlanEstudio.numPlan = PlanEstudio_Curso.numPlan)\n"
+                + "inner join Curso cur\n"
+                + "ON (PlanEstudio_Curso.codigoCurso = Cur.codigoCurso)"
+                + "WHERE Escuela_PlanEstudio.numPlan =" + numPlanC + " "
+                + "AND escuela.codigoEscuela ='" + codigoEscC + "';";
+        ResultSet res = Modelo.ConexionSQL.consulta(consulta);
+        while (res.next()) {
+            Vector v = new Vector();
+            v.add(res.getString(1));
+            v.add(res.getString(2));
+            v.add(res.getInt(3));
+            v.add(res.getInt(4));
+            v.add(res.getInt(5));
+            cursos += v.toString() + "\n";
+        }
+        String contenido = encabezado + cursos;
+        //Crea el documento pdf 
+        try {
+            FileOutputStream archivo = new FileOutputStream(Path + ".pdf");
+            Document doc = new Document();
+            PdfWriter.getInstance(doc, archivo);
+            doc.open();
+            doc.add(new Paragraph(contenido));
+            doc.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error " + e);
+        }
+
+        return Path + ".pdf";
+    }
+
+    //funcion que manda el correo 
+    private static boolean EnviarCorreo(String destinatario, String direccionArchivo) {
+        // Correo desde donde se va a mandar 
+        String username = "trabajoa.ATI@gmail.com";
+        String password = "nbt123nbt";
+
+        // Autenticación 
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        // Crear el email
+        MimeMessage msg = new MimeMessage(session);
+        try {
+            msg.setFrom(new InternetAddress("no-reply@gmail.com"));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));
+            // Título para el correo
+            msg.setSubject("Gestor de Planes de Estudio");
+
+            Multipart emailContent = new MimeMultipart();
+
+            // Crea el texto del mensaje
+            MimeBodyPart text = new MimeBodyPart();
+            text.setText("A continuación se adjunta la infomación del plan de estudios solicitados.");
+
+            // Carga el archivo adjunto
+            MimeBodyPart attachment = new MimeBodyPart();
+            attachment.attachFile(direccionArchivo);
+
+            // Agrega las partes al contenido
+            emailContent.addBodyPart(text);
+            emailContent.addBodyPart(attachment);
+
+            // Agrega el contenido al mensaje
+            msg.setContent(emailContent);
+
+            Transport.send(msg);
+            System.out.println("Sent message");
+        } catch (MessagingException e) {
+            System.out.println("Error al enviar el correo.");
+            return false;
+        } catch (IOException e) {
+            System.out.println("Error al intentar abrir el archivo");
+            return false;
+        }
+        return true;
+    }
+
+    public static void Envio(String Email) {
+        try {
+            String PATH = Crearpdf();
+            EnviarCorreo(Email, PATH);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ConConsultaPlan.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- Progra1POO/src/Vistas/ConsultaPlan.java
+++ Progra1POO/src/Vistas/ConsultaPlan.java
@@ -32,6 +32,10 @@ public class ConsultaPlan extends javax.swing.JFrame {
         txtCodEscuela = new javax.swing.JTextField();
         btnConsultar = new javax.swing.JButton();
         btnPDFEmail = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        LblTotalCursos = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        LblTotalCreditos = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
@@ -93,6 +97,27 @@ public class ConsultaPlan extends javax.swing.JFrame {
         });
 
         btnPDFEmail.setText("Generar PDF");
+        btnPDFEmail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPDFEmailActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("Leelawadee UI Semilight", 0, 14)); // NOI18N
+        jLabel4.setText("Total Cursos:");
+        jLabel4.setToolTipText("");
+
+        LblTotalCursos.setFont(new java.awt.Font("Leelawadee UI Semilight", 0, 14)); // NOI18N
+        LblTotalCursos.setText(" ");
+        LblTotalCursos.setToolTipText("");
+
+        jLabel5.setFont(new java.awt.Font("Leelawadee UI Semilight", 0, 14)); // NOI18N
+        jLabel5.setText("Total Creditos:");
+        jLabel5.setToolTipText("");
+
+        LblTotalCreditos.setFont(new java.awt.Font("Leelawadee UI Semilight", 0, 14)); // NOI18N
+        LblTotalCreditos.setText(" ");
+        LblTotalCreditos.setToolTipText("");
 
         jMenu2.setText("Salir");
 
@@ -141,6 +166,16 @@ public class ConsultaPlan extends javax.swing.JFrame {
                 .addGap(172, 172, 172)
                 .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(212, 212, 212))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(LblTotalCursos)
+                .addGap(105, 105, 105)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(LblTotalCreditos)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -161,7 +196,14 @@ public class ConsultaPlan extends javax.swing.JFrame {
                     .addComponent(btnPDFEmail))
                 .addGap(29, 29, 29)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(LblTotalCursos)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel5)
+                        .addComponent(LblTotalCreditos)))
+                .addGap(14, 14, 14))
         );
 
         pack();
@@ -190,6 +232,12 @@ public class ConsultaPlan extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Por favor revise los datos ingresados.");
         }
     }//GEN-LAST:event_btnConsultarActionPerformed
+
+    private void btnPDFEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPDFEmailActionPerformed
+        // TODO add your handling code here:
+        Controlador.ConConsultaPlan.AbrirEnviarPDF();
+        this.dispose();
+    }//GEN-LAST:event_btnPDFEmailActionPerformed
 
 //    /**
 //     * @param args the command line arguments
@@ -227,11 +275,15 @@ public class ConsultaPlan extends javax.swing.JFrame {
 //    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public static javax.swing.JLabel LblTotalCreditos;
+    public static javax.swing.JLabel LblTotalCursos;
     private javax.swing.JButton btnConsultar;
     private javax.swing.JButton btnPDFEmail;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem2;
